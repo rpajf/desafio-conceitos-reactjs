f7c113ec63156630e478323d4ec0f73dc2b5fdaa
Raphael Portela <rafaeljansenaraujo@gmail.com>
2020-09-03 19:50:41 -0300

    desafio completado

--- src/App.js
+++ src/App.js
@@ -20,8 +20,6 @@ function App() {
     setRepositories([...repositories, response.data]);
   }
 
-  
-
   async function handleRemoveRepository(id) {
     await api.delete(`repositories/${id}`);
 
@@ -35,13 +33,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
